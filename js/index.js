679e631605b51f3fa11d3705de42b6c9ff42d083
kpaul808 <kainoap808@gmail.com>
2016-06-24 11:55:05 -1000

    added text to repo

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var Uscore = 0;
+
+var highscore = 10;
+
+var press = function() {
+	Uscore++;
+	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+
+	if (Uscore > highscore) {
+		document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
+		return;
+	}
+};
+function myFunction() {
+    alert('DONE');
+}
